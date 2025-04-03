9ba6d11d895d8c9cd1a570ad5982085d773405f4
sowmika-avula <sowmika.avula@gmail.com>
2025-04-02 20:23:37 -0400

    Updated version to 1.0.1 and added build date

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
-VERSION = "1.0.0"
+VERSION = "1.0.1"
+BUILD_DATE = datetime.now().strftime("%Y-%m-%d")
 
 @app.route('/')
 def home():
@@ -26,6 +28,13 @@ def home():
                 }
                 h1 { color: #1a73e8; }
                 .version { color: #666; font-size: 0.9em; }
+                .build-info { 
+                    margin-top: 20px;
+                    padding-top: 10px;
+                    border-top: 1px solid #eee;
+                    font-size: 0.8em;
+                    color: #888;
+                }
             </style>
         </head>
         <body>
@@ -33,7 +42,10 @@ def home():
                 <h1>Welcome to DevOps Demo</h1>
                 <p>This is a simple Flask application containerized with Docker and deployed using GitHub Actions.</p>
                 <p>Created by: Sowmika Avula</p>
-                <p class="version">Version: """ + VERSION + """</p>
+                <div class="build-info">
+                    <p class="version">Version: """ + VERSION + """</p>
+                    <p>Build Date: """ + BUILD_DATE + """</p>
+                </div>
             </div>
         </body>
     </html>
